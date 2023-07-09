614e4c628ae415355e5eaf9aff68d0e51dbb9db1
r2d2 <arturo.galvis@hotmail.com>
2023-07-09 16:37:34 -0500

    Decal pendiente

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import {
   Environment,
   ContactShadows,
   SpotLight,
+  Decal,
+  useTexture,
   useGLTF,
   Center,
   AccumulativeShadows,
@@ -26,81 +28,88 @@ import { easing } from 'maath';
 export default function App() {
   const snap = useSnapshot(state);
   function Model(props) {
-    const { nodes, materials } = useGLTF('./models/libreta5.glb');
+    const { nodes, materials } = useGLTF('./models/libreta.glb');
+    const texture = useTexture(`three2.png`);
+    console.log(materials);
+    console.log(nodes);
     useFrame((state, delta) =>
       easing.dampC(materials.Material_0.color, snap.selectedColor, 0.25, delta)
     );
+
     // materials.Material_0.color = new THREE.Color(snap.selectedColor);
     return (
       <group {...props} dispose={null}>
-        <group position={[-0.064, -0.021, 0.034]} scale={0.007}>
-          <group position={[0, 0.5, 0]} rotation={[-Math.PI / 2, 0, 0]}>
-            <mesh
-              castShadow
-              receiveShadow
-              geometry={
-                nodes['Elástico(3DC1EE99-2D33-4495-8223-4BAA1260D758)'].geometry
-              }
-              material={materials.Material_2}
-            />
-            <mesh
-              castShadow
-              receiveShadow
-              geometry={
-                nodes['Front(05DAA8E6-C7CF-4657-92E9-EFD8F0005F06)'].geometry
-              }
-              material={materials.Material_0}
-            />
-            <mesh
-              castShadow
-              receiveShadow
-              geometry={
-                nodes['Front(077A05E9-2BD8-4B62-9B01-3387C604942A)'].geometry
-              }
-              material={materials.Material_0}
-            />
-            <mesh
-              castShadow
-              receiveShadow
-              geometry={
-                nodes['Front(119D16D1-4942-4A52-BD6A-DE874BB8F32D)'].geometry
-              }
-              material={materials.Material_0}
-            />
-            <mesh
-              castShadow
-              receiveShadow
-              geometry={
-                nodes['Front(3D83D045-8E56-4D70-886D-367F1D6F039B)'].geometry
-              }
-              material={materials.Material_0}
-            />
-            <mesh
-              castShadow
-              receiveShadow
-              geometry={
-                nodes['Front(5F1CFF6C-5BCC-450C-B831-39FC12063F55)'].geometry
-              }
-              material={materials.Material_0}
-            />
-            <mesh
-              castShadow
-              receiveShadow
-              geometry={
-                nodes['Front(78DF5E49-7C38-4F8F-A4C5-0D7C24FFB800)'].geometry
-              }
-              material={materials.Material_0}
-            />
-            <mesh
-              castShadow
-              receiveShadow
-              geometry={
-                nodes['Hojas(A52FF521-4CE0-45A5-BBB4-5174DAD361D2)'].geometry
-              }
-              material={materials.Material_1}
-            />
+        <mesh>
+          <group position={[-0.064, -0.021, 0.034]} scale={0.007}>
+            <group position={[0, 0.5, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+              <mesh
+                castShadow
+                receiveShadow
+                geometry={
+                  nodes['Elástico(3DC1EE99-2D33-4495-8223-4BAA1260D758)']
+                    .geometry
+                }
+                material={materials.Material_2}
+              />
+              <mesh
+                castShadow
+                receiveShadow
+                geometry={
+                  nodes['Front(05DAA8E6-C7CF-4657-92E9-EFD8F0005F06)'].geometry
+                }
+                material={materials.Material_0}
+              />
+              <mesh
+                castShadow
+                receiveShadow
+                geometry={
+                  nodes['Front(077A05E9-2BD8-4B62-9B01-3387C604942A)'].geometry
+                }
+                material={materials.Material_0}
+              />
+              <mesh
+                castShadow
+                receiveShadow
+                geometry={
+                  nodes['Front(119D16D1-4942-4A52-BD6A-DE874BB8F32D)'].geometry
+                }
+                material={materials.Material_0}
+              />
+              <mesh
+                castShadow
+                receiveShadow
+                geometry={
+                  nodes['Front(3D83D045-8E56-4D70-886D-367F1D6F039B)'].geometry
+                }
+                material={materials.Material_0}
+              />
+              <mesh
+                castShadow
+                receiveShadow
+                geometry={
+                  nodes['Front(5F1CFF6C-5BCC-450C-B831-39FC12063F55)'].geometry
+                }
+                material={materials.Material_0}
+              />
+              <mesh
+                castShadow
+                receiveShadow
+                geometry={
+                  nodes['Front(78DF5E49-7C38-4F8F-A4C5-0D7C24FFB800)'].geometry
+                }
+                material={materials.Material_0}
+              />
+              <mesh
+                castShadow
+                receiveShadow
+                geometry={
+                  nodes['Hojas(A52FF521-4CE0-45A5-BBB4-5174DAD361D2)'].geometry
+                }
+                material={materials.Material_1}
+              />
+            </group>
           </group>
-        </group>
+        </mesh>
       </group>
     );
   }
@@ -134,14 +143,31 @@ export default function App() {
   }
 
   function Customizer() {
+    //MESH color
     const colors = [
-      '#ccc',
-      '#EFBD4E',
-      '#80C670',
-      '#726DE8',
-      '#EF674E',
-      '#353934',
+      '#0077C8',
+      '#25282A',
+      '#F7EA48',
+      '#007749',
+      '#009639',
+      '#BA0C2F',
+      '#CE0F69',
+      '#6558b1',
+      '#3e342f',
+      '#ea733d',
+      '#002855',
+      '#2b2926',
     ];
+    //LEATHER color
+    const colors1 = [
+      '#A4343A',
+      '#f4f9ff',
+      '#789D4A',
+      '#425563',
+      '#707372',
+      '#2b2926',
+    ];
+
     const decals = ['./planelogo', 'journallogo', 'softlogo'];
 
     return (
