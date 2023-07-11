cf4761a42858cc0c7babb6afef86f8f7c5568b40
r2d2 <arturo.galvis@hotmail.com>
2023-07-10 22:30:34 -0500

    ultimo 10-07-2023

--- src/App.jsx
+++ src/App.jsx
@@ -48,6 +48,7 @@ export default function App() {
     console.log(materials);
 
     const [hovered, setHovered] = useState(null);
+
     useEffect(() => {
       const cursor = `<svg width="64" height="64" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0)"><path fill="rgba(255, 255, 255, 0.5)" d="M29.5 54C43.031 54 54 43.031 54 29.5S43.031 5 29.5 5 5 15.969 5 29.5 15.969 54 29.5 54z" stroke="#000"/><g filter="url(#filter0_d)"><path d="M29.5 47C39.165 47 47 39.165 47 29.5S39.165 12 29.5 12 12 19.835 12 29.5 19.835 47 29.5 47z" fill="${snap.items[hovered]}"/></g><path d="M2 2l11 2.947L4.947 13 2 2z" fill="#000"/><text fill="#000" style="white-space:pre" font-family="Inter var, sans-serif" font-size="10" letter-spacing="-.01em"><tspan x="35" y="63">${hovered}</tspan></text></g><defs><clipPath id="clip0"><path fill="#fff" d="M0 0h64v64H0z"/></clipPath><filter id="filter0_d" x="6" y="8" width="47" height="47" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/><feOffset dy="2"/><feGaussianBlur stdDeviation="3"/><feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.15 0"/><feBlend in2="BackgroundImageFix" result="effect1_dropShadow"/><feBlend in="SourceGraphic" in2="effect1_dropShadow" result="shape"/></filter></defs></svg>`;
       const auto = `<svg width="64" height="64" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill="rgba(255, 255, 255, 0.5)" d="M29.5 54C43.031 54 54 43.031 54 29.5S43.031 5 29.5 5 5 15.969 5 29.5 15.969 54 29.5 54z" stroke="#000"/><path d="M2 2l11 2.947L4.947 13 2 2z" fill="#000"/></svg>`;
@@ -67,7 +68,7 @@ export default function App() {
         {...props}
         dispose={null}
         onPointerOver={(e) => {
-          console.log(e.object.material.decal);
+          // console.log(e.object.material.name);
           setHovered(e.object.material.name);
         }}
         onPointerOut={(e) => {
@@ -94,25 +95,7 @@ export default function App() {
               material={materials.Material_2}
               material-color={snap.items.Material_2}
             />
-            <mesh
-              castShadow
-              receiveShadow
-              geometry={
-                nodes['Front(78DF5E49-7C38-4F8F-A4C5-0D7C24FFB800)'].geometry
-              }
-              material={materials.Material_0}
-              material-color={snap.items.Material_0}
-            >
-              <Decal
-                //debug // Makes "bounding box" of the decal visible
-                position={[3, 2, 100]}
-                rotation={[Math.PI / 2, 0, 0]}
-                scale={(30, 30, 30)}
-                // material-opacity={1}
-                // map-anisotropy={10}
-                material-map={texture}
-              />
-            </mesh>
+
             <mesh
               castShadow
               receiveShadow
@@ -158,7 +141,26 @@ export default function App() {
               material={materials.Material_0}
               //    material-color={snap.items.Material_0}
             />
-
+            <mesh
+              castShadow
+              receiveShadow
+              geometry={
+                nodes['Front(78DF5E49-7C38-4F8F-A4C5-0D7C24FFB800)'].geometry
+              }
+              material={materials.Material_0}
+              material-color={snap.items.Material_0}
+              // material-transparent={false}
+            >
+              <Decal
+                //debug // Makes "bounding box" of the decal visible
+                position={[3, 2, 100]}
+                rotation={[Math.PI / 2, 0, 0]}
+                scale={(30, 30, 30)}
+                // transparent={false}
+                //map-anisotropy={10}
+                material-map={texture}
+              />
+            </mesh>
             <mesh
               castShadow
               receiveShadow
@@ -286,7 +288,7 @@ export default function App() {
   return (
     <>
       <Canvas shadows camera={{ position: [-1, 0, 2.5], fov: 60 }}>
-        <axesHelper args={[1]} />
+        {/* <axesHelper args={[1]} /> */}
         <ambientLight intensity={0.5} />
         <Environment preset="city" />
         <Center>
